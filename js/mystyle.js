5daf0411f0f9d7e64615610c2feb6289d0acae23
Marisolenlaweb <marcarrimor@gmail.com>
2015-11-13 10:00:01 -0600

    initial commit

---
+++ js/mystyle.js
@@ -0,0 +1,10 @@
+//Entrada de ttítulo principal
+
+  $(function(){
+      $(".element").typed({
+        strings: ["Desarrollo Web & Marketing Digital"],
+        typeSpeed: 70,
+		showCursor: false,
+      });
+  });
+  
